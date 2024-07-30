4478e66828dbe1c8a12d36bec09fa8db13ac70f9
Umesh Yadav <umesh@shertechinfo.com>
2024-07-30 18:13:36 +0530

    few changes

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -9,7 +9,7 @@ export function AboutSection() {
         <div className={styles.mainCont}>
           <div className={styles.profilePicCont}>
             <img
-              src="profile.jpg"
+              src="Profile.jpg"
               alt=""
               className={styles.profilePic}
             />
